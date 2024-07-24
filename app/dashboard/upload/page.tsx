5b6b0c18ff769152dc2823777cf37c7dedfc8899
MaisonChetty <gamerelites16@gmail.com>
2024-07-24 18:39:38 +0200

    all

---
+++ app/dashboard/upload/page.tsx
@@ -0,0 +1,12 @@
+import FileUploader from '@/components/FileUploader'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <FileUploader/>
+    </div>
+  )
+}
+
+export default page
